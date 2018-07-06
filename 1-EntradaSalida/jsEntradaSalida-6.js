05d436b49d01e026eb9494590b8a5a2becf5a0c6
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-05 21:08:26 -0300

    Clase 2
    Clase 2 5/7/18 
    EntradaySalidaJs 5,6,8,9,10

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,29 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	/*var primerNumero;
+	var segundoNumero;
+	var resultado;
+	document.getElementById('numeroUno').value;
+	document.getElementById('numeroDos').value;
+	resultado = parseInt;*/
 
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	
+
+	resultado = numeroUno+numeroDos;
+	alert(resultado); 
+	/*El signo + sirve para Sumar y para Concatenar, cuando le pasamos un texto lo concatena, cuando es numero lo Suma
+	prompt y getelementarybyid devuelven texto
+	el parseint transforma el "2" en un 2, en la misma variable*/
+	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,16 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var numeroDividendo;
+	var numeroDvisor;
+	var resto;
+
+	numeroDividendo = document.getElementById('numeroDividendo').value;
+	numeroDividendo = parseInt(numeroDividendo);
+	numeroDvisor = document.getElementById('numeroDivisor').value;
+	numeroDvisor = parseInt(numeroDvisor);
+
+	resto = numeroDividendo%numeroDvisor;
+	alert(resto); 
+
 }
