73e25f59094085c1c19229ce759f4a7be2e883e5
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-09 01:26:20 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+	var precio;
+	var porcentajeDeDescuento;
+	var precioFinal;
+
+	precio = prompt("Precio: ");
+	porcentajeDeDescuento = prompt("Porcentaje de descuento: ");
+
+	precio = parseInt(precio);
+	porcentajeDeDescuento = parseInt(porcentajeDeDescuento);
+	precioFinal = parseInt(precioFinal);
+
+	precioFinal = -porcentajeDeDescuento*precio/100+precio;
+
+	document.getElementById('elPrecioFinal').value = precioFinal;//asi saca el resultado por Id
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var perimetro;
+
+ancho = prompt("Ingrese el ancho del rectangulo:");
+largo = prompt("Ingrese el largo del rectangulo:");
+
+ancho = parseInt(ancho);
+largo = parseInt(largo);
+
+perimetro = ancho*2+largo*2;
+
+perimetro = parseInt(perimetro);
+
+alert("Perimetro:" + perimetro);
+
+
 
 }
