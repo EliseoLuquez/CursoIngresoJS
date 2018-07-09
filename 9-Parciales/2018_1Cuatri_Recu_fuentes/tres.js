0957ca43c17ab8db3b5d594ed464ae73ea7d9a7a
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-09 01:27:02 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+	var precio;
+	var porcentajeDeDescuento;
+	var iva;
+	var precioConDescuento;
+	var descuentoEnDinero;
+	var precioFinal;
+
+	precio = prompt("Ingrese el precio: ");
+	porcentajeDeDescuento= prompt("Ingrese el porcentaje de descuento : ");
+	
+	precio = parseInt(precio);
+	porcentajeDeDescuento = parseInt(porcentajeDeDescuento);
+	iva = parseInt(iva);
+	descuentoEnDinero = parseInt(descuentoEnDinero);
+	precioConDescuento = parseInt(precioConDescuento);
+	precioFinal = parseInt(precioFinal);
+
+
+	descuentoEnDinero = precio*porcentajeDeDescuento/100;
+	precioConDescuento = precio-descuentoEnDinero; 
+	iva = precio*0.21;
+
+	alert("El descuento en dinero es: "+descuentoEnDinero+" , el precio con descuento es: "+precioConDescuento+" , el IVA es: "+iva);
+	precioFinal = precioConDescuento+iva;
+
+	document.getElementById('elPrecioFinal').value = precioFinal;
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,24 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base = prompt("Ingrese la base del triangulo equilatero: ");
+	altura = prompt("Ingrese la altura del triangulo equilatero: ");
+
+	base = parseInt(base);
+	altura = parseInt(altura);
+
+	superficie = base*altura/2;
+	perimetro = base*3;
+
+	alert("La superficie del triangulo equilatero es "+superficie+ " y el perimetro es "+perimetro);
+
+
+
+
 
 }
