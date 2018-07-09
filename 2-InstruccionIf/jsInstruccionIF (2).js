5a194742d51b5428dd2cd684e40285c2d76ee8d4
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-09 19:47:31 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad 
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if(edad>=18)
+	{
+		alert("Eres mayor de edad");
+	} 
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if(edad>17)
+	{
+		alert("Eres mayor de edad");
+	}else
+	{
+		alert("Eres menor de edad");
+	}
 
 
 }//FIN DE LA FUNCIÓN
