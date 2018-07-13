11d79d6406d327cebf94ef20aeab7aa20a8807c0
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-12 21:28:34 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -3,32 +3,25 @@ function mostrar()
 	//Genero el número RANDOM entre 1 y 10 
 	var numero;
 
-	numero = (Math.floor(Math.random() * 10)+ 1);
+	numero = (Math.floor(Math.random() * 11)+ 1);
 
 	if (numero>=9)
-
-		{
-			alert(numero+" EXCELENTE");
-		} 
-			else {
-					if(numero>=4&&numero<9)
-						{
-							alert(numero+" APROBO");
-						}
-							else {
-									if(numero<4)
-										{
-											alert(numero+ " Vamos, la proxima se puede");
-										}
-							}
-
-
-
-
-
-			}	
-
-
+	{
+	alert(numero+" EXCELENTE");
+	} 
+	
+	else {
+	if(numero>=4&&numero<9)
+	{
+	alert(numero+" APROBO");
+	
+	else {
+	if(numero<4)
+	{
+	alert(numero+ " Vamos, la proxima se puede");
+	}
+	
+	}
 
 
 }//FIN DE LA FUNCIÓN
