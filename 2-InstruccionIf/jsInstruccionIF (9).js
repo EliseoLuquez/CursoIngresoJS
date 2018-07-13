36a577d67b413de5e39493ecbd07cc6371152bfb
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-12 21:21:18 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,8 +2,16 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-
-	alert(Math.floor(Math.random() * 10)+ 1);
+	var numero;
+	//alert(Math.floor(Math.random() * 10)+ 1); Ejercicio
 	
+	/*
+	numero = Math.floor(Math.random() * max-min) + min; 
+
+	*/
+	//Funcion random
+	numero = Math.floor(Math.random() * (100-50)) + 50;
+
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
