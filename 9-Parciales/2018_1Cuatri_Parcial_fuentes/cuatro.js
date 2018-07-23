055ffa59ba98fd8e2df72e0d602fc37c3762c70b
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-23 16:50:06 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var resta;
+
+
+	numeroUno=prompt("Ingrese un numero");
+	numeroDos=prompt("Ingrese un numero");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	suma=(numeroUno+numeroDos);
+	suma=parseInt(suma);
+	resta=parseInt(resta);
+	resta=numeroUno-numeroDos;
+
+	if(numeroUno==numeroDos)
+	{
+		alert(numeroUno++numeroDos);
+	}else
+	{if(numeroUno>numeroDos)
+	{
+		alert(resta);
+	}
+
+	}
+
 
 }
