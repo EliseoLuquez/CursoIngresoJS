f7c78884d3c93d2d4695ccf140e3fb322d0d53b6
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-23 22:18:20 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,33 +10,71 @@ E. Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
-  var precioUnitario;
+  var precioBruto;
+  var precioFinal;
   var precio;
-  var lamparas;
-  var precioDescuento;
+  var marca;
+  var precioConDescuento;
   var cantidad;
   var descuento;
   var iibb;
   var precioIibb;
-  lamparas = document.getElementById('Marca').value;
+  marca = document.getElementById('Marca').value;
   cantidad = document.getElementById('Cantidad').value;
-  precioDescuento = parseInt(precioDescuento);
+  precioConDescuento = parseInt(precioConDescuento);
   cantidad = parseInt(cantidad);
+  precioFinal = parseInt(precioFinal);
+  precioBruto = parseInt(precioBruto);
   precio = parseInt(precio);
-  precioUnitario = parseInt(precioUnitario);
-  precioUnitario = 35;
   descuento = parseInt(descuento); 
   iibb = parseInt(iibb);
-  iibb = 0.1;
   precioIibb = parseInt(precioIibb);
+  precio = 35;
+  iibb = 0.1;
+  descuento = 1;
+  precioBruto = precio*cantidad;
+  //Hacer pruebas de escritorio
+  switch(cantidad){ 
+        case 1:
+              precioFinal;
+              break;
+  switch(marca){
+        case 2:
+              "ArgentinaLuz";  
+              descuento=0.85;
+              break;
+      
+  }            
 
+
+
+
+
+
+
+  }
+  precioFinal=precioBruto*descuento;
+  document.getElementById('precioDescuento').value = precioFinal;
+  
+
+
+
+
+
+
+
+
+
+
+
+  /*
   if(cantidad==6) //A. Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
   {
     precio = cantidad*precioUnitario;
     descuento = 0.5;
     precioDescuento = precio*descuento;
     document.getElementById('precioDescuento').value = precioDescuento;
-  } else {
+  } else 
   if(cantidad>6)
   {
     precio = cantidad*precioUnitario;
@@ -45,15 +83,13 @@ function CalcularPrecio ()
     precioIibb = precioDescuento*iibb;
     document.getElementById('precioDescuento').value = precioDescuento;
     alert("Usted pago " +precioIibb+" de ingresos brutos");    
-  } else {//B. Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % 
-  if(cantidad==5&&lamparas=="ArgentinaLuz")
+  } else if(cantidad==5&&lamparas=="ArgentinaLuz")//B. Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 %
   {
     precio = cantidad*precioUnitario;
     descuento = 0.6;
     precioDescuento = precio*descuento;
     document.getElementById('precioDescuento').value = precioDescuento;
-  } else {//y si es de otra marca el descuento es del 30%. 
-  if(cantidad==5&&lamparas!="ArgentinaLuz"|| precioDescuento>=120)
+  } else if(cantidad==5&&lamparas!="ArgentinaLuz"|| precioDescuento>=120)//y si es de otra marca el descuento es del 30%. 
   {
     precio = cantidad*precioUnitario;
     descuento = 0.7;
@@ -61,49 +97,37 @@ function CalcularPrecio ()
     document.getElementById('precioDescuento').value = precioDescuento;
     precioIibb = precioDescuento*iibb;
     alert("Usted pago " +precioIibb+" de ingresos brutos"); 
-  } else{//Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % 
-  if(cantidad==4&&lamparas=="ArgentinaLuz"||cantidad==4&&lamparas=="FelipeLamparas")
+  } else if(cantidad==4&&lamparas=="ArgentinaLuz"||cantidad==4&&lamparas=="FelipeLamparas")
   {
     precio = cantidad*precioUnitario;
     descuento = 0.75;
     precioDescuento = precio*descuento;
     document.getElementById('precioDescuento').value = precioDescuento;
-  } else {//y si es de otra marca el descuento es del 20%.
-  if(cantidad==4&&lamparas=="JeLuz"||cantidad==4&&lamparas=="Osram"||cantidad==4&&lamparas=="HazIluminacion")
+  } else if(cantidad==4&&lamparas=="JeLuz"||cantidad==4&&lamparas=="Osram"||cantidad==4&&lamparas=="HazIluminacion")//y si es de otra marca el descuento es del 20%.
   {
     precio = cantidad*precioUnitario;
     descuento = 0.8;
     precioDescuento = precio*descuento;
     document.getElementById('precioDescuento').value = precioDescuento;
-  } else {//D.Si compra 3  lamparitas, si es de otra marca un 5%.
-  if(cantidad==3&&lamparas=="JeLuz"||cantidad==3&&lamparas=="HazIluminacion"||cantidad==3&&lamparas=="Osram")
+  } else if(cantidad==3&&lamparas=="JeLuz"||cantidad==3&&lamparas=="HazIluminacion"||cantidad==3&&lamparas=="Osram")//D.Si compra 3  lamparitas, si es de otra marca un 5%.
   {
     precio = cantidad*precioUnitario;
     descuento = 0.95;
     precioDescuento = precio*descuento;
     document.getElementById('precioDescuento').value = precioDescuento;
-  } else {//si es  “FelipeLamparas” se hace un descuento del 10 %
-  if(cantidad==3&&lamparas=="FelipeLamparas")
+  } else if(cantidad==3&&lamparas=="FelipeLamparas")//si es  “FelipeLamparas” se hace un descuento del 10 %
   {
     precio = cantidad*precioUnitario;
     descuento = 0.9;
     precioDescuento = precio*descuento;
     document.getElementById('precioDescuento').value = precioDescuento;
-  } else {//Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%
-  if(cantidad==3&&lamparas=="ArgentinaLuz")
+  } else if(cantidad==3&&lamparas=="ArgentinaLuz") //Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15% 
   {
     precio = cantidad*precioUnitario;
     descuento = 0.85;
     precioDescuento = precio*descuento;
     document.getElementById('precioDescuento').value = precioDescuento;
   } 
-  }
-  }
-  }
-  }
-  }
-  }  
-  } 
-  }
-
+  */
+  
 }
--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -1,5 +1,4 @@
 var eleccionMaquina;
-var eleccionJugador;
 var contadorDeEmpates=0;
 var contadorDeGanadas=0;
 var contadorDePerdidas=0;
@@ -8,84 +7,77 @@ function comenzar()
 {
 	eleccionMaquina=Math.floor(Math.random() * 3)+1;//Genero el número RANDOM entre 1 y 3
 	//alert(eleccionMaquina);	 	
-	eleccionJugador = prompt("Elija un numero:Piedra(1), papel(2) o tijera(3) ");
+	
 
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
-{
-	if(eleccionMaquina==1&&eleccionJugador==1)
+{	
+	if(eleccionMaquina==1)
 	{
 		alert("Has empatado!");
-		contadorDeEmpates=contadorDeEmpates+1;
-	}else
-	{
-	if(eleccionMaquina==1&&eleccionJugador==2)
+		contadorDeEmpates++;
+	}else if(eleccionMaquina==3)
 	{
 		alert("Has ganado!");
-		contadorDeGanadas=contadorDeGanadas+1;
-	}else
-	{
-	if(eleccionMaquina==1&&eleccionJugador==3)
+		contadorDeGanadas++;
+	}else 
 	{
 		alert("Has perdido!");
-		contadorDePerdidas=contadorDePerdidas+1;
+		contadorDePerdidas++;
 	}
-	}
-	}
+	comenzar();
+	mostrarResultado();
+	
+	
 }//FIN DE LA FUNCIÓN
 function papel()
-{
-	if(eleccionMaquina==2&&eleccionJugador==2)
+{	
+	if(eleccionMaquina==2)
 	{
 		alert("Has empatado!");
-		contadorDeEmpates=contadorDeEmpates+1;
-	}else
-	{
-	if(eleccionMaquina==1&&eleccionJugador==2)
+		contadorDeEmpates++;
+	}else if(eleccionMaquina==1)
 	{
 		alert("Has ganado!");
-		contadorDeGanadas=contadorDeGanadas+1;
-	}else
-	{
-	if(eleccionMaquina==3&&eleccionJugador==2)
+		contadorDeGanadas++;
+	}else 
 	{
 		alert("Has perdido!");
-		contadorDePerdidas=contadorDePerdidas+1;
+		contadorDePerdidas++;
 	}
-	}
-	}
+	comenzar();
+	mostrarResultado();
+	
+	
 }//FIN DE LA FUNCIÓN
 function tijera()
-{
-	if(eleccionMaquina==3&&eleccionJugador==3)
+{	
+	
+	if(eleccionMaquina==3)
 	{
 		alert("Has empatado!");
-		contadorDeEmpates=contadorDeEmpates+1;
-	}else
-	{
-	if(eleccionMaquina==2&&eleccionJugador==3)
+		contadorDeEmpates++;
+	}else if(eleccionMaquina==2)
 	{
 		alert("Has ganado!");
-		contadorDeGanadas=contadorDeGanadas+1;
+		contadorDeGanadas++;
 	}
 	else
 	{
-	if(eleccionMaquina==1&&eleccionJugador==3)
-	{
 		alert("Has perdido!");
-		contadorDePerdidas=contadorDePerdidas+1;
+		contadorDePerdidas++;
 	}
-	}
-	}
+	comenzar();
+	mostrarResultado();
 }//FIN DE LA FUNCIÓN
 
 function mostrarResultado()
 {
-	
 	document.getElementById('ganadas').value=contadorDeGanadas;
 	document.getElementById('perdidas').value=contadorDePerdidas;
 	document.getElementById('empatadas').value=contadorDeEmpates;
-	
+
 }
+
