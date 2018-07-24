ec5e92fa7b93aee3faef250caa32b6163274a430
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-23 22:19:05 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,46 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
+/*
+	Evalua una expresion en un conjunto de valores
+	valores :
+	switch(expresion){
+	case 1:
+	{
+		codigo...;
+		break;
+	}		
+	case 2:
+	{
+		codigo...;
+		break; Para la ejecucion!
+	}
+	default:				
+	}		
+*/
+switch(mes){
+	case "Enero":
+	{
+		alert("Comienza bien el año!!");
+		break;
+	}
+	case "Marzo":
+	{
+		alert("A clases!!");		
+		break;
+	}	
+	case "Julio":
+	{
+		alert("Se vienen las vacaciones!!");
+		break;
+	}
+	case "Diciembre":
+	{
+		alert("Felices fiestas!!");
+		break;						
+	}		
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,6 +4,36 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+switch(mesDelAño){
+	case "Febrero":
+	
+		alert("Este mes no tiene mas de 29 dias");
+		break;
+	default;
+			
+	
+/*	case "Enero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	case "Julio":
+	case "Agosto":
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+		*/
+		alert("Este mes tiene 30 dias o  mas");
+		
+	
+
+
+
+
+
+
+}
 	
 	
 
