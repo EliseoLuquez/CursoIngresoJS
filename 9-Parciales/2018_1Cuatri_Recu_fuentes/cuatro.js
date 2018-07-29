99f21942cc1b4b80faad21b2bdf0c8923429f8a6
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-29 19:14:26 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+/*(IF)Pedir dos números y mostrar el resultado: 
+Si son iguales los muestro concatenados. 
+Si el primero es mayor, los divido, 
+de lo contrario los sumo. 
+Si la suma es menor a 50 ,además de mostrar el resultado, muestro el mensaje 
+"la suma es xxx y es menor a 50".
+*/
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var divison;
+	suma=numeroUno+numeroDos;
+	divison=numeroUno/numeroDos;
+    numeroUno=prompt("Ingrese un numero");
+    numeroDos=prompt("Ingrese un numero");
+    numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	sum=parseInt(sum)
+	if(numeroUno==numeroDos)
+	{
+        alert("numeroUno"+"numeroDos");
+	}
+	else if(numeroUno>numeroDos)
+	{
+		alert(divison);
+	}
+	else if(numeroUno<numeroDos)
+	{
+		alert(suma)	
+	}else(suma>50)
+	{
+		alert("La suma es "+suma+" y es mayor a 50");
+	}
 
 }
