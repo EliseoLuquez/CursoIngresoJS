e76d58bd31bb7e20381e75a6ee9b814dd7ad2072
Eliseo <40706146+EliseoLuquez@users.noreply.github.com>
2018-07-30 11:51:05 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+/*Bienvenidos. 
+(SWITCH)pedir el ingreso de un día de la semana, 
+si es fin de semana mostrar " buen finde", 
+si es día hábil “ a trabajar”, 
+si no es un día valido, también informarlo, 
+usar una sola ventana alert.*/
+	
+	var dias;
+	dias=prompt("Ingrese un dia de la semana");
+	switch(dias)
+	{
+		case "lunes":
+		case "martes":
+		case "miercoles":
+		case "jueves":
+		case "viernes":
+			alert("A trabajar!");
+			break;
+		case "sabado":
+		case "domingo":
+			alert("Buen finde!");
+			break;
+		default:
+			alert("No es un dia valido");	
+	}
+
+
+
+
+
+
+
 
 }
